89d3358944e796c6aca080c37cd4d49ecc08de3e
adil <adilahmed3886@gmail.com>
2024-10-13 16:49:22 +0500

    user controller

--- src/app.js
+++ src/app.js
@@ -4,22 +4,16 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true //send cookies with requests
-}))
+app.use(cors({origin: process.env.CORS_ORIGIN,credentials: true}))
 
-app.use(express.json({
-    limit: "16kb" //sends json data with a limit of 16kb in post or put
-}));
-
-app.use(express.urlencoded({
-    extended: true,
-    limit: "16kb"
-}));
-
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//routes:
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
